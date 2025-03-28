a95e5c6aa67bafa1e2f409e056d18f2ce935fbca
Jakob Schlüpmann <89900439+schlupmann@users.noreply.github.com>
2025-03-28 16:30:34 +0100

    Update saml20-idp-remote.php

--- app/Config/simplesamlphp/metadata/saml20-idp-remote.php
+++ app/Config/simplesamlphp/metadata/saml20-idp-remote.php
@@ -1,1 +1,12 @@
+<?php
 
+// To configure see : https://simplesamlphp.org/docs/stable/simplesamlphp-reference-idp-remote.html
+
+/* The index of the array is the entity ID of this IdP. */
+$metadata['entity-id-1'] = [
+    /* Configuration options for the first IdP. */
+];
+
+$metadata['entity-id-2'] = [
+    /* Configuration options for the second IdP. */
+];
